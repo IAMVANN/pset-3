fcc1ea8c9c5c72c68ade7bba51cb2001b6312953
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-10-28 10:49:22 -0400

    finished half of exercise 1

--- src/change.js
+++ src/change.js
@@ -4,11 +4,33 @@ const MIN = 0.01;
 const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
-
+    const changeValue=[25,10,5,1]; //[quaters, dimes, nickels, pennies]
+    let quarters;
+    let dimes;
+    let nickels;
+    let pennies;
+    const variableHolder=[quarters, dimes, nickels, pennies];
+    let type;
+    let typeNumber;
+    let boolean;
+    let changeLeft=amount*100;
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+      console.log("\nInvalid.");
 } else {
-    // write your code for making change here
-}
+  for (let whichOne=0;whichOne <=3; whichOne++){
+    type=changeValue[whichOne];
+    typeNumber=variableHolder[whichOne];
+    if(changeLeft>=type){
+        typeNumber=(changeLeft-changeLeft%type)/type;
+        changeLeft=changeLeft%type;
+        console.log(typeNumber);
+      } else {
+        typeNumber=0;
+        console.log(typeNumber);
+      }
+      //I finished first part of this problem, now i need to do code for quarter/quater depending on quaters=1 or something like that.
+    }
+
+  }
