3aa51ca19ab04ced45b97a79b1e9983c3bdb90e4
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-11-01 09:37:55 -0400

    FInished exercise 2

--- src/sign.js
+++ src/sign.js
@@ -3,13 +3,7 @@ const number=Number(Scanner.question("\nEnter a number: ").split(",").join(""));
 let decimalFinder=String(number);
 decimalFinder=decimalFinder.includes(".");
 let nonNumbersFinder=String(number);
-console.log(number);
-/*if(nonNumbersFinder.includes(0)==true || nonNumbersFinder.includes(1)==true || nonNumbersFinder.includes(2)==true || nonNumbersFinder.includes(3)==true || nonNumbersFinder.includes(4)==true || nonNumbersFinder.includes(5)==true || nonNumbersFinder.includes(6)==true || nonNumbersFinder.includes(7)==true || nonNumbersFinder.includes(8)==true || nonNumbersFinder.includes(9)==true){
-  nonNumbersFinder=false;
-} else {
-  nonNumbersFinder=true;
-}*/
-if(number>Number.MAX_SAFE_INTEGER || number<Number.MIN_SAFE_INTEGER || decimalFinder==true || number==NaN ){//I NEED TO USE ISNAB
+if(number>Number.MAX_SAFE_INTEGER || number<Number.MIN_SAFE_INTEGER || decimalFinder==true || Number.isNaN(number)==true ){
   console.log("\nInvalid.");
 } else {
   if(number==0){
