89f2f23a7ee345f16cf8ff127e2c889b7967094d
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-11-05 10:01:35 -0500

    I did exercise 5

--- src/gpa.js
+++ src/gpa.js
@@ -1,29 +1,42 @@
 const scanner = require("readline-sync");
-const letterGrade = scanner.question("\nEnter a letter grade: ");
+let letterGrade = scanner.question("\nEnter a letter grade: ");
 let addOrMinus=0;
 let valid=true;
 let grade;
+let fail=true;
 let gradeArray=["a","A", "b", "B", "c", "C","d","D","f","F"];
 let concurrentGradeValueArray=[4.00, 4.00, 3.00, 3.00, 2.00, 2.00, 1.00, 1.00, 0.00, 0.00];
-if(letterGrade.includes("+"){
+if (letterGrade.includes("F+")||letterGrade.includes("F-")||letterGrade.includes("f+")||letterGrade.includes("f-")){
+  valid=false;
+}
+if(letterGrade.includes("+")){
   addOrMinus=.33;
   letterGrade=letterGrade.replace("+", "");
 } else if (letterGrade.includes("-")){
   addOrMinus=-.33;
   letterGrade=letterGrade.replace("-", "");
-} else if (letterGrade.includes("+").includes("-")){
+} else if (letterGrade.includes("+") && letterGrade.includes("-")){
   valid=false;
-  letterGrade=letterGrade.replace("+", "").replace("-","");
+  letterGrade=letterGrade.replace("+", "") && letterGrade.replace("-","");
 }
 for(let whichOne=0; whichOne<=9; whichOne++){
   if(gradeArray[whichOne]===letterGrade){
-    grade=concurrentGradeValueArray[whichOne]
+    grade=concurrentGradeValueArray[whichOne];
+    fail=false;
   }
 }
-
+if(fail==true){
+  valid=false;
+}
+if(letterGrade!="a" && letterGrade!="A" ){
+  grade=grade+addOrMinus;
+} else if (letterGrade=="a" || letterGrade=="A"){
+  if (addOrMinus<0){
+    grade=grade+addOrMinus;
+  }
 }
 if(valid===false){
-  console.log("Invalid.");
+  console.log("\nInvalid.");
 } else{
-  console.log("Your GPA is " + grade + ".")
+  console.log("\nYour GPA is " + grade + ".")
 }
